0a8dde2db63a2c120944f0616a2205042865c062
abder <abder@Abam1826.speedport.ip>
2023-12-26 20:31:48 +0100

    erweiterung, but prob with getHashIndex

--- ALG&DAT/src/p33/MyHashTableWithChaining.java
+++ ALG&DAT/src/p33/MyHashTableWithChaining.java
@@ -20,4 +20,75 @@ public class MyHashTableWithChaining<K,V> {
 	public int size() {return size;}
 	public boolean isEmpty() {return size()==0;}
 
+	public V get(K key) {
+		int bucketIndex =getHashIndex(key);
+		HashNode<K,V>head = bucketArray.get(bucketIndex);
+		while(head !=null) {
+			if(head.key.equals(key)) {
+				return head.value;	
+			}
+			head=head.next;
+		}
+		return null;
+	}
+	
+	public void add(K key,V value) {
+		int bucketIndex =getHashIndex(key);
+		HashNode<K,V>head = bucketArray.get(bucketIndex);
+		while(head !=null) {
+			if(head.key.equals(key)) {
+				head.value=value;
+				return;
+			}
+			head=head.next;
+		}
+		size++;
+		head=bucketArray.get(bucketIndex);
+		HashNode<K,V> newNode=new HashNode<K,V>(key,value);
+		newNode.next=head;
+		bucketArray.set(bucketIndex, newNode);
+	}
+	
+	public V remove(K key ) {
+		int bucketIndex=getHashIndex(key);
+		HashNode<K,V>head =bucketArray.get(bucketIndex);
+		HashNode<K,V> prev =null;
+		while(head!=null) {
+			if(head.key.equals(key)) {
+				size--;
+				if(prev!=null)
+					prev.next=head.next;
+				else
+					bucketArray.set(bucketIndex, head.next);
+				return head.value;
+			}
+			prev=head;
+			head=head.next;
+		}
+		return null;
+	}
+	
+	public void add(K key , V value) {
+		size++;
+		head=bucketArray.get(numBuckets);
+		HashNode <K,V>newNode =new HashNode<K,V>(key,value);
+		newNode.next=head;
+		bucketArray.set(bucketIndex,newNode);
+		
+		if(1.0*size/numBuckets >=0.7) {
+			ArrayList<HashNode<K,V>>temp=bucketArray;
+			bucketArray =new ArrayList<>();
+			numBuckets=2*numBuckets;
+			size=0;
+			for(int i=0;i<numBuckets;i++) {
+				bucketArray.add(null);
+			}
+			for(HashNode<K,V>headNode:temp) {
+				while(headNode!=null) {
+					add(headNode.key,headNode.value);
+					headNode=headNode.next;
+				}
+			}
+		}
+	}
 }
